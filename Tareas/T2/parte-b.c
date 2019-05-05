6ad64552c09bb748557cb3cb8b5a859da3cee078
cmesiasd <cmesiasd@gmail.com>
2019-05-04 22:59:42 -0400

    T2 Part a finished

---
+++ Tareas/T2/parte-b.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "t2.h"
+
+#define MAX 80
+
+void read_line(FILE *file, char *line, int i, int len);
+int cmp_lines(void *a, int k, void *x);
+
+void read_line(FILE *file, char *line, int i, int len) {
+  if (fseek(file, i*MAX, SEEK_SET)!=0) {
+    perror("compare_file_lines");
+    exit(1);
+  }
+  if (fread(line, 1, len, file)!=len) {
+    perror("compare_file_lines");
+    exit(1);
+  }
+}
+
+int cmp_lines(void *a, int k, void *x){
+	FILE *f = a;
+	char *s = x;
+	int len = strlen(s);
+	char pa[len];
+	read_line(f, pa, k, len);
+	int val = strcmp(pa, s);
+	return val;
+}
+
+char *consultar(char *nom_dic, char *pal, int n_lin) {
+	FILE *f = fopen(nom_dic, "r");
+	return buscar(f, n_lin, pal, cmp_lines);
+}
